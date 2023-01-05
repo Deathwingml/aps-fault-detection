7a03fa332d8801926c1d1eb4b0a34e04881aa0f3
deathwingml <you@example.compardesisantoshsingh@gmail.com>
2023-01-05 21:11:09 +0530

    data ingestion is completed

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -1,20 +1,42 @@
 # We need to create these classes for our project since it's specific to our problem statement
 # pass is same as ...
 
+import os, sys
+from sensor.exception import SensorException
+from sensor.logger import logging
+from datetime import datetime
+
+FILE_NAME = "sensor.csv"
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+TRANSFORMER_OBJECT_FILE_NAME = "transformer.pkl"
+TARGET_ENCODER_OBJECT_FILE_NAME = "target_encoder.pkl"
+MODEL_FILE_NAME = "model.pkl"
+
 
 
 class TrainingPipelineConfig:
 
     def __init__(self):
-        self.artifact_dir = os.path.join(os.getcwd(), "artifact, f"{datetime.now().strftime('%m%d%Y_%H%M%D')""})
+        try:
+            self.artifact_dir = os.path.join(os.getcwd(), "artifact", f"{datetime.now().strftime('%m%d%Y_%H%M%S')}")
+        except Exception as e:
+            raise SensorException(e,sys)
 
 
 class DataIngestionConfig:
 
     def __init__(self, training_pipeline_config:TrainingPipelineConfig):
-        self.database_name = "aps"
-        self.collection_name = "sensor"
-        self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir,"data_ingestion")
+        try:
+            self.database_name = "aps"
+            self.collection_name = "sensor"
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir,"data_ingestion")
+            self.feature_store_dir = os.path.join(self.data_ingestion_dir, "feature_store", FILE_NAME)
+            self.train_file_path = os.path.join(self.data_ingestion_dir, "dataset", TRAIN_FILE_NAME)
+            self.test_file_path = os.path.join(self.data_ingestion_dir, "dataset", TEST_FILE_NAME)
+            self.test_size = 0.2
+        except Exception as e:
+            raise SensorException(e,sys)
 
 
     
